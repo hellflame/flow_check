cd726799645b44d19ec937e1b5aeb114cfe00027
hellflame <hellflamedly@gmail.com>
2015-10-03 14:27:10 +0800

    platform declare

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FlowCheck',
-    version='0.2',
+    version='0.3',
     keywords=('quantity of flow', 'flow check', 'flow'),
     description="在linux终端基于ip命令获取统计通过网卡的流量信息",
     license='MIT',
@@ -13,7 +13,7 @@ setup(
     author_email="hellflamedly@gmail.com",
     url='https://github.com/hellflame/flow_check',
     packages=find_packages(),
-    platforms='linux',
+    platforms='Ubuntu',
     entry_points={
         'console_scripts': [
             'flower=flow.flower:main'
