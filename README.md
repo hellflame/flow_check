5ac693fcc06831232303bb90b5605c1f64885fff
hellflame <hellflamedly@gmail.com>
2015-10-20 15:05:55 +0800

    说明信息

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-# iplocate
+# FlowCheck
+
+ip 命令截取
+
+## 安装
+
+```bash
+  $ sudo pip install flowCheck --upgrade
+```
+
+## 调用
+
+```bash
+  $ flower
+  $ flower -h 
+  $ flower -l
+  $ flower -a
+  $ flower eth0
+
+```
+## 说明
 
 + 基于linux系统中的ip命令
 + ip 命令作用和ifconfig相似,然而并不需要管理员权限
@@ -8,6 +28,7 @@
 
 
 
->> 历史版本
+## 历史版本
 
 + 0.1 英文输出, 未封装, 裸方法, linux中ubuntu, CentOs 可以使用ip命令
++ 0.4 添加说明说明文件
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 # coding=utf8
-__author__ = 'hellflame'
 
 from setuptools import setup, find_packages
+__author__ = 'hellflame'
+
 
 setup(
     name='FlowCheck',
-    version='0.3',
+    version='0.4',
     keywords=('quantity of flow', 'flow check', 'flow'),
     description="在linux终端基于ip命令获取统计通过网卡的流量信息",
     license='MIT',
@@ -14,6 +15,14 @@ setup(
     url='https://github.com/hellflame/flow_check',
     packages=find_packages(),
     platforms='Ubuntu',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7"
+    ],
     entry_points={
         'console_scripts': [
             'flower=flow.flower:main'
